56b48c065d7df36c2a73381c81f3f6f6b1f613b3
komova_a <fillgood33@gmail.com>
2024-10-03 22:08:26 +0300

    I hope this is my last commit

---
+++ locators/data.py
@@ -0,0 +1,7 @@
+class EndpointURLs:
+    MAIN_PAGE = 'https://stellarburgers.nomoreparties.site/'
+    FEED = 'https://stellarburgers.nomoreparties.site/feed'
+    REGISTER = 'https://stellarburgers.nomoreparties.site/api/auth/register'
+    LOGIN = 'https://stellarburgers.nomoreparties.site/login'
+    PROFILE = 'https://stellarburgers.nomoreparties.site/account/profile'
+    USER = 'https://stellarburgers.nomoreparties.site/api/auth/user'
